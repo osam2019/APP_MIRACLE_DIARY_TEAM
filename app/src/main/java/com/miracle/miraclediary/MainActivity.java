5abbaa78828963867f788323d25d941618b27434
lhsljh123 <56831352+lhsljh123@users.noreply.github.com>
2019-10-21 22:20:53 +0900

    second commit
    test

--- app/src/main/java/com/miracle/miraclediary/MainActivity.java
+++ app/src/main/java/com/miracle/miraclediary/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //테스트 주석입니다 신나다.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
