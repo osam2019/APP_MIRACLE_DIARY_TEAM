87b645eb6fbcb76a79006e6986dda1e1cdf7da9e
snack <snack>
2019-10-22 12:04:03 +0900

    add editor popup

--- app/src/main/java/com/miracle/miraclediary/EditorActivity.java
+++ app/src/main/java/com/miracle/miraclediary/EditorActivity.java
@@ -1,8 +1,7 @@
 package com.miracle.miraclediary;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +9,15 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.miracle.miraclediary.dialog.EditorTutorialDialog;
+
 public class EditorActivity extends AppCompatActivity {
 
+    private final String INIT_STR = "할 일을 적어볼까요? :)";
     private boolean __DEBUG__ = true;
-
     private Button SubmitButton;
     private EditText ContextEditText;
-
     private String m_context;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +25,16 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
 
         SetEvents();
+        CreatePopUp();
     }
 
+    //이벤트 초기설정 함수입니다.
     private void SetEvents() {
         //등록 버튼
         SubmitButton = findViewById(R.id.bt_submit);
         //내용
         ContextEditText = findViewById(R.id.editText_context);
+        ContextEditText.setText(INIT_STR);
 
         //등록버튼의 이벤트 리스너
         SubmitButton.setOnClickListener(new View.OnClickListener() {
@@ -45,10 +47,10 @@ public class EditorActivity extends AppCompatActivity {
         ContextEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus) {
-                    UpdateConText(null);
-                    if(m_context.equals("일기를 적어볼까요 :)")) {
-                        UpdateConText("");
+                if (hasFocus) {
+                    UpdateContext(null);
+                    if (m_context.equals(INIT_STR)) {
+                        UpdateContext("");
                     }
                 }
             }
@@ -57,8 +59,22 @@ public class EditorActivity extends AppCompatActivity {
 
     }
 
+    //팝업이 생성되는 함수입니다.
+    private void CreatePopUp() {
+        //차 후 인텐트를 이용한 데이터 공유 시 수정
+        Intent intent = getIntent();
+
+
+        if (true) {
+            Intent intent_popup = new Intent(EditorActivity.this, EditorTutorialDialog.class);
+            startActivityForResult(intent_popup, 1);
+        }
+
+    }
+
+    //작성 버튼을 눌렀을 시 발생하는 함수입니다.
     public void SubmitContext() {
-        UpdateConText(null);
+        UpdateContext(null);
 
         if (__DEBUG__) {
             Log.d("Test", m_context);
@@ -67,9 +83,10 @@ public class EditorActivity extends AppCompatActivity {
         //여기서부터 db에 저장하시면 됩니다
     }
 
-    public void UpdateConText(String str) {
+    //작성 중인 일기 내용을 여러 방면으로 업데이트하는 함수입니다.
+    public void UpdateContext(String str) {
         m_context = str == null ? ContextEditText.getText().toString() : str;
-        if(str != null) {
+        if (str != null) {
             ContextEditText.setText(str);
         }
     }
